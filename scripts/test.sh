722c48039852f9ea4e5dda4f7d4cc8b13f37284a
Ehtesham Hasnain <33456936+ehasnain@users.noreply.github.com>
2021-06-27 02:21:11 +0200

    fix(test.sh): fix missing combine-report when test fails

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+# set e+x
+
 $(yarn bin)/cypress run --reporter cypress-multi-reporters --reporter-options configFile=reporter-config.json
+
+# cache exit code for file exit
+EXIT_CODE=$?
+
+# run posttest script
+yarn combine-reports
+
+# exit with cached exit code
+exit $EXIT_CODE
